9a7d30d24f9b20499a63ab0192dde93aedcb722d
cole-campbell <cole.william.campbell@gmail.com>
2016-04-05 16:00:07 +0100

    Tested out Grunt automation with Uglify & Sass.

---
+++ x.1-GruntSass/gruntfile.js
@@ -0,0 +1,38 @@
+
+module.exports = function(grunt) {
+	//Grunt Configuration of Grunt Packages
+	grunt.initConfig({
+		sass: {                              // Task 
+    			dist: {                            // Target 
+      				options: {                       // Target options 
+        				style: 'compressed'
+      				},
+      				files: {                         // Dictionary of files 
+        				'<% filename %>.sass': 'style.css'       // 'destination': 'source'        				
+      				}
+    			}
+  			}
+		pkg: grunt.file.readJSON('package.json'),
+			uglify: {
+				build:{
+					src:'src/*.js',
+					dest:'build/.min.js'
+				}*/
+			
+	//Sass
+
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jade');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	//grunt.loadNpmTasks('grunt-sass');
+
+	grunt.registerTask('default', ['uglify']);
+	grunt.registerTask('default', ['sass']);
+	//grunt.registerTask('default', ['sass']);
+
+};
+
+*/
